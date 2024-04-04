6d2b0eb5545223256ca3145048c64b57156cdd75
Мария Орлеан <gr412_ormal@lab-407-7.tomtit-tomsk.ru>
2024-04-04 15:39:34 +0700

    first commit

---
+++ ImageUploader/ImageUploader.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controller;
+
+class ImageUploader
+{
+    public function upload(array $image): string
+    {
+        if (!empty($image['image'])) {
+            $root = app()->settings->getRootPath();
+            $path = $_SERVER['DOCUMENT_ROOT']. $root. '/public/img/';
+            $name = $image['image']['name'];
+
+            move_uploaded_file($image['image']['tmp_name'], $path. $name);
+
+            return $name;
+        }
+
+        return '';
+    }
+}
