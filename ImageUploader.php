802501c365cd25ee3961301570a93248213e5c40
Мария Орлеан <gr412_ormal@lab-407-7.tomtit-tomsk.ru>
2024-04-04 16:44:01 +0700

    .

--- ImageUploader.php
+++ ImageUploader.php
@@ -11,16 +11,11 @@ class ImageUploader
         if (!empty($image['image'])) {
             $root = \app()->settings->getRootPath();
             $path = $_SERVER['DOCUMENT_ROOT']. $root. '/public/img/';
-            $extension = pathinfo($image['image']['name'], PATHINFO_EXTENSION);
-            $randomName = uniqid() . '.' . $extension;
+            $name = $image['image']['name'];
 
-            if (!file_exists($path)) {
-                mkdir($path, 0777, true);
-            }
+            move_uploaded_file($image['image']['tmp_name'], $path. $name);
 
-            move_uploaded_file($image['image']['tmp_name'], $path. $randomName);
-
-            return $randomName;
+            return $name;
         }
 
         return '';
